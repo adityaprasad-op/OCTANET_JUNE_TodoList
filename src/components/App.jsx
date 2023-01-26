7777d0b931edf2fca2abf14184358018a20f10df
adityaprasad-op <aditya.prasad2002@gmail.com>
2023-01-26 23:32:58 +0530

    strikethrough

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,44 @@
-import React from "react";
+import React,{useState} from "react";
+import ToDoItem from "./ToDoItem";
 
 function App() {
+  const [text,setText]=useState("");
+  const [items,setItems]=useState([]);
+
+  function handleChange(event){
+    const newValue=event.target.value;
+    setText(newValue);
+  }
+
+  function addItem(){
+    setItems((prevItems)=>{
+      return[...prevItems,text];
+    });
+    setText("");
+  }
+
+  function deleteItem(){
+
+  }
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} type="text" value={text}/>
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {items.map(todoItem=> 
+          <ToDoItem
+            text={todoItem}
+            onChecked={deleteItem}
+          />
+          )}
         </ul>
       </div>
     </div>
---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,17 @@
+import React,{useState} from "react";
+
+function ToDoItem(props){
+    const [isDone,setIsDone]=useState(false);
+
+    function handleClick(){
+        setIsDone(prevValue=>{
+            return !prevValue;
+        });
+    }
+
+    return <div onClick={handleClick}>
+            <li style={{textDecoration:isDone?"-through":"none"}}>{props.text}</li>
+    </div>
+}
+
+export default ToDoItem; 
